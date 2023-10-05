6373114b840741a9c3e8850850660213ed9530c7
MoroccoBS <chouaibwidadipur@gmail.com>
2023-10-05 11:57:39 +0100

    Fixed pycodestyle

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,10 @@ import sys
 if __name__ == "__main__":
     args = sys.argv[1:]
     num_args = len(args)
+    text = "argument" if num_args == 1 else "arguments"
     if num_args == 0:
         print("0 arguments.")
     else:
-        print("{} {}:".format(num_args, "argument" if num_args == 1 else "arguments"))
+        print("{} {}:".format(num_args, text))
         for i, arg in enumerate(args, start=1):
             print("{}: {}".format(i, arg))
