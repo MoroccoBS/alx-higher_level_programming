d670f14da48093947633d0280606a2d7a463720a
MoroccoBS <chouaibwidadipur@gmail.com>
2023-10-12 02:26:32 +0100

    Added 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string: str):
+    mapping = {
+        "M": 1000,
+        "CM": 900,
+        "D": 500,
+        "CD": 400,
+        "C": 100,
+        "XC": 90,
+        "L": 50,
+        "XL": 40,
+        "X": 10,
+        "IX": 9,
+        "V": 5,
+        "IV": 4,
+        "I": 1,
+    }
+    integer = 0
+    for i in range(len(roman_string)):
+        if (
+            i < len(roman_string) - 1
+            and mapping[roman_string[i]] < mapping[roman_string[i + 1]]
+        ):
+            integer -= mapping[roman_string[i]]
+        else:
+            integer += mapping[roman_string[i]]
+
+    return integer
