54295a95a19e190f3a25c958432e35d77f003a6e
MoroccoBS <chouaibwidadipur@gmail.com>
2023-10-12 01:32:41 +0100

    Added 8-simple_delete.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-print_sorted_dictionary = __import__(
-    "6-print_sorted_dictionary"
-).print_sorted_dictionary
-
-
 def update_dictionary(a_dictionary: dict, key: str, value: any):
     new_dict = a_dictionary.copy()
     new_dict[key] = value
