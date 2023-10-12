15c1c2ef41c8b597f696ae374e1ff828d3a549fc
MoroccoBS <chouaibwidadipur@gmail.com>
2023-10-12 01:48:43 +0100

    Added 9-multiply_by_2.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,25 +1,5 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
-    if not isinstance(key, str):
-        return None
-    a_dictionary[key] = value
-    return a_dictionary
-
-
-print_sorted_dictionary = __import__(
-    "6-print_sorted_dictionary"
-).print_sorted_dictionary
-
-a_dictionary = {"language": "C", "number": 89, "track": "Low level"}
-new_dict = update_dictionary(a_dictionary, "language", "Python")
-print_sorted_dictionary(new_dict)
-print("--")
-print_sorted_dictionary(a_dictionary)
-
-print("--")
-print("--")
-
-new_dict = update_dictionary(a_dictionary, "city", "San Francisco")
-print_sorted_dictionary(new_dict)
-print("--")
-print_sorted_dictionary(a_dictionary)
+def update_dictionary(a_dictionary: dict, key: str, value: any):
+    new_dict = a_dictionary.copy()
+    new_dict[key] = value
+    return new_dict
