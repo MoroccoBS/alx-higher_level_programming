b3a746e5412628a185c7e940cd261bb239a1c1ae
MoroccoBS <chouaibwidadipur@gamil.com>
2023-10-26 01:08:22 +0100

    Added 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a Square class"""
+
+
+class Square:
+    """A class that defines a square"""
+
+    __size = 0
+
+    def __init__(self, size):
+        """Initialize a new square size"""
+        self.__size = size
