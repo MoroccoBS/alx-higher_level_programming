7ed94a4fc519831c5cfd5fde59a2858ba34e4a08
MoroccoBS <chouaibwidadipur@gamil.com>
2023-10-30 14:34:11 +0100

    Update

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,48 +1,22 @@
 #!/usr/bin/python3
-"""This module is composed by a class that defines a Rectangle"""
+"""Defines a Rectangle class"""
 
 
 class Rectangle:
-    """Class that defines a rectangle"""
+    """Represent a rectangle"""
 
     def __init__(self, width=0, height=0):
-        """Method that initializes the instance
-
-        Args:
-            width: width of the rectangle
-            height: height of the rectangle
-
-
-        """
+        """Initialize a new Rectangle..."""
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """method that returns the value of the width
-
-        Returns:
-            width of the rectangle
-
-
-        """
-
         return self.__width
 
     @width.setter
     def width(self, value):
-        """method that defines the width
-
-        Args:
-            value: width
-
-        Raises:
-            TypeError: if width is not an integer
-            ValueError: if width is less than zero
-
-
-        """
-
+        """Initialize a new rectangle width"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -51,30 +25,11 @@ class Rectangle:
 
     @property
     def height(self):
-        """method that returns the value of the height
-
-        Returns:
-            height of the rectangle
-
-
-        """
-
         return self.__height
 
     @height.setter
     def height(self, value):
-        """method that defines the height
-
-        Args:
-            value: height
-
-        Raises:
-            TypeError: if height is not an integer
-            ValueError: if height is less than zero
-
-
-        """
-
+        """Initialize a new rectangle height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
