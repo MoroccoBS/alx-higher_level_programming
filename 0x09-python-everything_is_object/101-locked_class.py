9dc605f2ba6de1e9488fff6feb744c87b596f344
MoroccoBS <chouaibwidadipur@gamil.com>
2023-10-31 19:39:52 +0100

    Update

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""A class that prevents the user from instantiating new attributes"""
+
+
 class LockedClass:
+    """A class that prevents the user from instantiating new attributes"""
+
     __slots__ = ["first_name"]
