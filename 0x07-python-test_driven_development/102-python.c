6cab0a17fcc7970fccc72fe9b96c60783848a895
MoroccoBS <chouaibwidadipur@gamil.com>
2023-10-31 19:59:17 +0100

    Added c

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,30 @@
+#include <python3.10/Python.h>
+
+/**
+ * print_python_string - prints information about Python string objects
+ * @p: a Python string object
+ * Return: VOID
+ */
+
+void print_python_string(PyObject *p)
+{
+	long int length;
+
+	fflush(stdout);
+
+	printf("[.] string object info\n");
+	if (!PyUnicode_Check(p))
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+
+	length = ((PyASCIIObject *)(p))->length;
+
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
+	printf("  length: %ld\n", length);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
+}
