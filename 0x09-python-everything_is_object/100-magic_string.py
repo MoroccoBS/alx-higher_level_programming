0148c456cd27d75f0e52f1416808ce5d02e5be96
MoroccoBS <chouaibwidadipur@gamil.com>
2023-10-31 15:51:07 +0100

    Update

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-# def magic_string():
-#     magic_string.n = 0
-#     magic_string.n += 1
-#     return "BestSchool, " * (magic_string.n - 1) + "BestSchool"
 def magic_string():
     magic_string.count = getattr(magic_string, "count", 0)
     magic_string.count += 1
