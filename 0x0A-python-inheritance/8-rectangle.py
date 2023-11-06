7b1a477de943411c51bc6a83a7b3aa5e0fbd1d6a
MoroccoBS <chouaibwidadipur@gamil.com>
2023-11-06 21:33:08 +0100

    Added 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""Rectangle module"""
 
 
 class Rectangle:
     """class Rectangle"""
 
-    pass
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
