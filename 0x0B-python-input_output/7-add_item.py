016224a9e36985422154bd2eaf741d0ee27e7018
MoroccoBS <chouaibwidadipur@gamil.com>
2023-11-07 21:53:08 +0100

    Added 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""function that adds all arguments to a Python list"""
+
+import json
+import sys
+
+
+def add_item_json_file():
+    """adds all arguments to a Python list"""
+    args = sys.argv[1:]
+    with open("add_item.json", "a") as f:
+        if len(args) > 0:
+            json.dump(args, f)
+        else:
+            json.dump([], f)
