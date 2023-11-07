73b5b447b8a48115f7ce49b8ef2ec95dd1213457
MoroccoBS <chouaibwidadipur@gamil.com>
2023-11-07 21:41:25 +0100

    Fixed 3 & 2

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines a function that writes a string to a text file (UTF8) and returns the number of characters written"""
+"""function that writes a string to a text file"""
 
 
 def write_file(filename="", text=""):
