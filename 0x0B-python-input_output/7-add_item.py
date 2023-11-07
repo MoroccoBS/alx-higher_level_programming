8b5456cbcb10f846ed447fffb4c2cf2033484a73
MoroccoBS <chouaibwidadipur@gamil.com>
2023-11-07 22:00:36 +0100

    Added 9

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """function that adds all arguments to a Python list"""
-
-import json
 import sys
+
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 
 
 def add_item_json_file():
     """adds all arguments to a Python list"""
     args = sys.argv[1:]
-    with open("add_item.json", "a") as f:
-        if len(args) > 0:
-            json.dump(args, f)
-        else:
-            json.dump([], f)
+    my_list = load_from_json_file("add_item.json")
+    for arg in args:
+        my_list.append(arg)
+    save_to_json_file(my_list, "add_item.json")
 
 
 if __name__ == "__main__":
